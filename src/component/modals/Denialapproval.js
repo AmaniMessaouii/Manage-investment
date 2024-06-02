442b9508b83ae39835a3bc28b2e8d3756cb57ba6
AmaniMessaouii <messaoui.amani15@gmail.com>
2024-06-02 01:48:48 +0100

    fixing radio btn

--- src/component/modals/Denialapproval.js
+++ src/component/modals/Denialapproval.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { Badge, Button, Checkbox, Input, Modal, Table } from 'antd';
 import NotifModel from './NotifModel';
 
